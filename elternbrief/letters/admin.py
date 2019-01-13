9832df118d483a1578b81241bffae4f60ff6ab44
David Besau <david.besau@gmail.com>
2019-01-13 23:21:37 +0100

    Create basic models

--- elternbrief/letters/admin.py
+++ elternbrief/letters/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Group, ClassGroup, Letter, Student
 
-# Register your models here.
+admin.site.register(Group)
+admin.site.register(ClassGroup)
+admin.site.register(Letter)
+admin.site.register(Student)
--- elternbrief/letters/models.py
+++ elternbrief/letters/models.py
@@ -1,3 +1,42 @@
 from django.db import models
+from django.utils import timezone
+from django.conf import settings
 
-# Create your models here.
+
+class Group(models.Model):
+    name = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.name
+
+
+class ClassGroup(models.Model):
+    name = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.name
+
+
+class Letter(models.Model):
+    name = models.CharField(max_length=30)
+    date_published = models.DateField("Veröffentlichungsdatum", default=timezone.now)
+    date_due = models.DateField("Fällig bis", blank=True, null=True)
+    teacher = models.CharField("Zuständige Lehrkraft", max_length=30, blank=True)
+    confirmation = models.BooleanField("Muss bestätigt werden", default=True)
+    groups_concerned = models.ManyToManyField(Group, blank=True)
+    classes_concerned = models.ManyToManyField(ClassGroup, blank=True)
+    document = models.FileField(upload_to='documents/%Y/%m/%d/')
+
+    def __str__(self):
+        return self.name
+
+
+class Student(models.Model):
+    parents = models.ManyToManyField(settings.AUTH_USER_MODEL, verbose_name="Erziehungsberechtigte", blank=True)
+    first_name = models.CharField("Vorname", max_length=30)
+    last_name = models.CharField("Nachname", max_length=30)
+    class_group = models.ForeignKey(ClassGroup, verbose_name="Klasse", on_delete=models.PROTECT)
+    groups = models.ManyToManyField(Group, verbose_name="Gruppen", blank=True)
+
+    def __str__(self):
+        return self.first_name + self.last_name + ", " + str(self.class_group)
