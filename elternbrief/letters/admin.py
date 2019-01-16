fc1560663511fcd64f3e9fa41e0661c8c7650ce3
David Besau <david.besau@gmail.com>
2019-01-16 18:57:00 +0100

    Reimplement parent-child relation

--- elternbrief/letters/admin.py
+++ elternbrief/letters/admin.py
@@ -1,7 +1,24 @@
 from django.contrib import admin
-from .models import Group, ClassGroup, Letter, Student
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.models import User
+
+from .models import Group, ClassGroup, Letter, Student, Parent
 
 admin.site.register(Group)
 admin.site.register(ClassGroup)
 admin.site.register(Letter)
 admin.site.register(Student)
+
+
+class ParentInline(admin.StackedInline):
+    model = Parent
+    can_delete = False
+    verbose_name = "Elternteil"
+
+
+class UserAdmin(BaseUserAdmin):
+    inlines = (ParentInline,)
+
+
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
--- elternbrief/letters/models.py
+++ elternbrief/letters/models.py
@@ -1,6 +1,9 @@
 from django.db import models
 from django.utils import timezone
 from django.conf import settings
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 
 class Group(models.Model):
@@ -32,7 +35,7 @@ class Letter(models.Model):
 
 
 class Student(models.Model):
-    parents = models.ManyToManyField(settings.AUTH_USER_MODEL, verbose_name="Erziehungsberechtigte", blank=True)
+    # parents = models.ManyToManyField(settings.AUTH_USER_MODEL, verbose_name="Erziehungsberechtigte", blank=True)
     first_name = models.CharField("Vorname", max_length=30)
     last_name = models.CharField("Nachname", max_length=30)
     class_group = models.ForeignKey(ClassGroup, verbose_name="Klasse", on_delete=models.PROTECT)
@@ -40,3 +43,19 @@ class Student(models.Model):
 
     def __str__(self):
         return self.first_name + " " + self.last_name + ", " + str(self.class_group)
+
+
+class Parent(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    children = models.ManyToManyField(Student, verbose_name="Kinder", blank=True)
+
+
+@receiver(post_save, sender=User)
+def create_user_parent(sender, instance, created, **kwargs):
+    if created:
+        Parent.objects.create(user=instance)
+
+
+@receiver(post_save, sender=User)
+def save_user_parent(sender, instance, **kwargs):
+    instance.parent.save()
