fd4bf37eb073b2bd032c4b9e6601ecd566279cac
David Besau <david.besau@gmail.com>
2019-01-16 17:30:28 +0100

    Add login functionality

--- elternbrief/letters/views.py
+++ elternbrief/letters/views.py
@@ -1,14 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login as dj_login
 
 
 def index(request):
     context = {}
 
-    if not request.user.is_authenticated:
-        return render(request, 'letters/index.html', context)
-    else:
+    if request.user.is_authenticated:
         return render(request, 'letters/index_logged_in.html', context)
+    else:
+        return render(request, 'letters/index.html', context)
 
 
 def login(request):
-    return render(request, 'letters/login.html')
+    if 'username' in request.POST.keys() and 'password' in request.POST.keys():
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            dj_login(request, user)
+            return redirect('letters:index')
+        else:
+            context = {'badlogin': True}
+            return render(request, 'letters/login.html', context=context)
+
+    else:
+        return render(request, 'letters/login.html')
