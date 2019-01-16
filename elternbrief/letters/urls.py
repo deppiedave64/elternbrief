56f7162c911b15db8c12d6c77869bac840b562c9
David <david.besau@gmail.com>
2019-01-16 16:40:49 +0100

    Add basic dummy interface

--- elternbrief/letters/urls.py
+++ elternbrief/letters/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 app_name = 'letters'
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('login/', views.login, name='login')
 ]
--- elternbrief/letters/views.py
+++ elternbrief/letters/views.py
@@ -3,4 +3,12 @@ from django.shortcuts import render
 
 def index(request):
     context = {}
-    return render(request, 'letters/index.html', context)
+
+    if not request.user.is_authenticated:
+        return render(request, 'letters/index.html', context)
+    else:
+        return render(request, 'letters/index_logged_in.html', context)
+
+
+def login(request):
+    return render(request, 'letters/login.html')
