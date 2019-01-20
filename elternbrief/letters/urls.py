32e19f24c08a572c258a3936ae036e92ef0f77f6
David <david.besau@gmail.com>
2019-01-20 14:21:05 +0100

    Add pdf to detail view

--- elternbrief/letters/urls.py
+++ elternbrief/letters/urls.py
@@ -1,4 +1,7 @@
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
+
 from . import views
 
 app_name = 'letters'
@@ -8,4 +11,4 @@ urlpatterns = [
     path('logout/', views.logout, name='logout'),
     path('letters/', views.letters, name='letters'),
     path('letters/<int:letter_id>/', views.letter_detail, name='letter_detail'),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
