55c0e1dbe43049be4d87dbb59039bc4b08c1283e
David <david.besau@gmail.com>
2019-01-21 12:12:20 +0100

    Remove unneeded import

--- elternbrief/letters/models.py
+++ elternbrief/letters/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 from django.utils import timezone
-from django.conf import settings
 from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.dispatch import receiver
