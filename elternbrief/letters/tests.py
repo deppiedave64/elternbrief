1c17554cf221b076a8831dfe3fcf542781adf48a
David Besau <david.besau@gmail.com>
2019-04-30 23:58:37 +0200

    Add first test case for Letter.by_student

--- elternbrief/letters/tests.py
+++ elternbrief/letters/tests.py
@@ -1,3 +1,25 @@
+import django
 from django.test import TestCase
+from unittest.mock import MagicMock
 
-# Create your tests here.
+from .models import Student, Letter, ClassGroup
+
+class LetterModelTests(TestCase):
+
+    def test_by_student_with_right_class(self):
+        """ by_student() returns a letter, if the student is in the class the letter concerns."""
+
+        test_class_group = ClassGroup(name="Test class")
+        test_class_group.save()
+        test_student = Student(first_name="John", last_name="Doe", class_group=test_class_group)
+        test_student.save()
+
+        test_document = MagicMock(spec=django.core.files.File, name="Test document")
+        test_document.name = 'test.pdf'
+
+        test_letter = Letter(name="Test letter", document=test_document)
+        test_letter.save()
+        test_letter.classes_concerned.add(test_class_group)
+        test_letter.save()
+
+        self.assertIn(test_letter, Letter.by_student(test_student))
