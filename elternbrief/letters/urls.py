878dfdbb48444f704b3ac676d62b12f65c6cf7bc
deppiedave64 <david.besau@protonmail.com>
2019-07-31 19:36:24 +0200

    Only serve uploaded files as static files if DEBUG is True

--- elternbrief/letters/urls.py
+++ elternbrief/letters/urls.py
@@ -6,13 +6,16 @@ from . import views
 
 app_name = 'letters'
 urlpatterns = [
-                  path('', views.index, name='index'),
-                  path('login/', views.login, name='login'),
-                  path('logout/', views.logout, name='logout'),
-                  path('letters/', views.letters, name='letters'),
-                  path('letters/<int:student_id>/<int:letter_id>/', views.letter_detail, name='letter_detail'),
-                  path('letters/<int:student_id>/<int:letter_id>/confirm/', views.letter_detail, {'confirmation': True},
-                       name='letter_confirm'),
-                  path('letters/results/<int:letter_id>/', views.letter_result, name='letter_result')
-              ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    path('', views.index, name='index'),
+    path('login/', views.login, name='login'),
+    path('logout/', views.logout, name='logout'),
+    path('letters/', views.letters, name='letters'),
+    path('letters/<int:student_id>/<int:letter_id>/', views.letter_detail, name='letter_detail'),
+    path('letters/<int:student_id>/<int:letter_id>/confirm/', views.letter_detail, {'confirmation': True},
+         name='letter_confirm'),
+    path('letters/results/<int:letter_id>/', views.letter_result, name='letter_result')
+]
+
 # Allow uploaded files to be served as static files. Should be changed when going into production.
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
